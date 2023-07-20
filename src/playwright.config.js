44712222e81a61a798c650a347815a4894bd8c1d
Gideon <git@mortolian.com>
2023-07-20 15:22:18 +0200

    FIX: Fix playwright config issue. Has to have one worker or it seems to have inconsistent results.

--- src/playwright.config.js
+++ src/playwright.config.js
@@ -5,8 +5,8 @@ module.exports = defineConfig({
     testDir: './tests/e2e',
     fullyParallel: true,
     forbidOnly: !!process.env.CI,
-    retries: process.env.CI ? 2 : 0,
-    workers: process.env.CI ? 1 : undefined,
+    retries: process.env.CI ? 1 : 0,
+    workers: process.env.CI ? 1 : 1,
     reporter: 'html',
 
     projects: [
@@ -22,14 +22,14 @@ module.exports = defineConfig({
             name: 'webkit',
             use: {...devices['Desktop Safari']},
         },
-        // {
-        //     name: 'Mobile Chrome',
-        //     use: {...devices['Pixel 5']},
-        // },
-        // {
-        //     name: 'Mobile Safari',
-        //     use: {...devices['iPhone 12']},
-        // },
+        {
+            name: 'Mobile Chrome',
+            use: {...devices['Pixel 5']},
+        },
+        {
+            name: 'Mobile Safari',
+            use: {...devices['iPhone 12']},
+        },
     ],
 
     webServer: {
