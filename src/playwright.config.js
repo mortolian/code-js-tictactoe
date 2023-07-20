e4dce5531b212a8bbf5a681cc1906ec07d18dda7
Gideon <git@mortolian.com>
2023-07-20 15:03:30 +0200

    FIX: Added new tests to e2e testing and fixed the playwright problems.

--- src/playwright.config.js
+++ src/playwright.config.js
@@ -5,8 +5,8 @@ module.exports = defineConfig({
     testDir: './tests/e2e',
     fullyParallel: true,
     forbidOnly: !!process.env.CI,
-    retries: process.env.CI ? 2 : 0,
-    workers: process.env.CI ? 1 : undefined,
+    retries: process.env.CI ? process.env.CI : 0,
+    workers: process.env.CI ? process.env.CI : 1,
     reporter: 'html',
 
     projects: [
--- src/tests/e2e/tictactoe.spec.js
+++ src/tests/e2e/tictactoe.spec.js
@@ -8,6 +8,8 @@ test('has title', async ({ page }) => {
 
 test('click the new game button', async({ page }) => {
     await page.goto('./');
+    await page.locator('#block-0').click();
+    await page.locator('#block-4').click();
     await page.getByText('New Game').click();
     await expect(page.locator('#block-0')).toBeEmpty();
     await expect(page.locator('#block-1')).toBeEmpty();
@@ -18,4 +20,70 @@ test('click the new game button', async({ page }) => {
     await expect(page.locator('#block-6')).toBeEmpty();
     await expect(page.locator('#block-7')).toBeEmpty();
     await expect(page.locator('#block-8')).toBeEmpty();
+});
+
+test('X Player Wins', async({ page }) => {
+    await page.goto('./');
+    await page.locator('#block-0').click();
+    await page.locator('#block-4').click();
+    await page.locator('#block-1').click();
+    await page.locator('#block-5').click();
+    await page.locator('#block-2').click();
+    await expect(page.locator('#outcome')).toHaveText(/The WINNER is player X/);
+});
+
+test('O Player Wins', async({ page }) => {
+    await page.goto('./');
+    await page.locator('#block-0').click();
+    await page.locator('#block-4').click();
+    await page.locator('#block-1').click();
+    await page.locator('#block-2').click();
+    await page.locator('#block-7').click();
+    await page.locator('#block-6').click();
+    await expect(page.locator('#outcome')).toHaveText(/The WINNER is player O/);
+});
+
+test('Game ends in a draw', async({ page }) => {
+    await page.goto('./');
+    await page.locator('#block-0').click();
+    await page.locator('#block-4').click();
+    await page.locator('#block-1').click();
+    await page.locator('#block-2').click();
+    await page.locator('#block-6').click();
+    await page.locator('#block-3').click();
+    await page.locator('#block-5').click();
+    await page.locator('#block-8').click();
+    await page.locator('#block-7').click();
+    await expect(page.locator('#outcome')).toHaveText(/This game is a DRAW!/);
+});
+
+test('Game has a winner even when 9 games are played', async({ page }) => {
+    await page.goto('./');
+    await page.locator('#block-0').click();
+    await page.locator('#block-4').click();
+    await page.locator('#block-3').click();
+    await page.locator('#block-6').click();
+    await page.locator('#block-7').click();
+    await page.locator('#block-5').click();
+    await page.locator('#block-1').click();
+    await page.locator('#block-8').click();
+    await page.locator('#block-2').click();
+    await expect(page.locator('#outcome')).toHaveText(/The WINNER is player X/);
+});
+
+test('Check that the player turn indicator changes correctly.', async({ page }) => {
+    await page.goto('./');
+    await expect(page.locator('#x-player')).toHaveClass(/selected/);
+    await page.locator('#block-0').click();
+    await expect(page.locator('#o-player')).toHaveClass(/selected/);
+    await page.locator('#block-1').click();
+    await expect(page.locator('#x-player')).toHaveClass(/selected/);
+});
+
+test('Test if the GitHub link in the footer works', async ({ page }) => {
+    await page.goto('./');
+    const pagePromise = page.waitForEvent('popup');
+    await page.getByRole('link', { name: 'Code Available on GitHub.' }).click();
+    const githubPage = await pagePromise;
+    console.log(await githubPage.title());
 });
