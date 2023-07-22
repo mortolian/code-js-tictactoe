3a1a802bae7af8c0bf1bf4a0c9b0e7170e60fb3a
Gideon <git@mortolian.com>
2023-07-22 13:45:13 +0200

    REMOVE: All vitests, tictactoe is more effectively tested using e2e testing.

--- tests/unit/tictactoe.spec.js
+++ tests/unit/tictactoe.spec.js
@@ -1,10 +1,5 @@
-import { expect, test } from 'vitest'
-import '../../src/components/tictactoe.js'
+import {expect, test} from 'vitest'
 
 test('Sample Test - Should work as expected', () => {
     expect(Math.sqrt(4)).toBe(2)
-})
-
-test('Toggle Player', () => {
-    
 })
